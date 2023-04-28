4ec5a85b697634ecf9a1357b25caef65f8af98ae
Divy Raushan <hs68217@gmail.com>
2023-04-28 13:16:55 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,93 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  fG!: FormGroup;
+  updateIndex!: any
+  toDoTaskUpdate:boolean = false;
+  inProgressUpdate:boolean = false
+
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.fG = this.fb.group({
+      addedTask: ['', Validators.required]
+    })
+  }
+
   title = 'Todo';
+  todoTask: Array<any> = []
+  progressTask: Array<string> = []
+  doneTask: Array<string> = []
+
+
+
+  addTask() {
+    this.todoTask.push(this.fG.value.addedTask);
+    this.fG.reset();
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  editTodo(item: string, i: number) {
+    this.fG.controls['addedTask'].setValue(item)
+    this.toDoTaskUpdate = true
+    this.updateIndex = i
+  }
+
+  updateTask() {
+
+    if(this.toDoTaskUpdate)
+    {
+      this.todoTask[this.updateIndex] = this.fG.value.addedTask;
+      this.fG.reset();
+      this.toDoTaskUpdate = false;
+      this.updateIndex = undefined;
+    }
+    else if(this.inProgressUpdate)
+    {
+     this.progressTask[this.updateIndex] = this.fG.value.addedTask;
+     this.fG.reset();
+     this.inProgressUpdate = false;
+     this.updateIndex = undefined;
+    }
+    
+  }
+
+  deleteTodo(i: number) {
+    this.todoTask.splice(i, 1)
+  }
+
+  editProgress(item:any, i: any) {
+
+    this.fG.controls['addedTask'].setValue(item);
+    this.inProgressUpdate = true;
+    this.updateIndex = i;
+
+  }
+
+  deleteProgress(i: number) {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +23,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    ReactiveFormsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    FormsModule,
+    DragDropModule,
+    MatIconModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
